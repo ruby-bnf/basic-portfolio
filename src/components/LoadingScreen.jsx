83db7b0f2158665ccb0bcb175364c407a8482bea
Ruby bnf <r.barnerfontyn@gmail.com>
2025-08-06 23:53:25 +0200

    Update loading animatinon add skip button and and logic

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,52 +2,58 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const [step, setStep] = useState(0);
 
-  const text1 = "<Looking... for the meaning off life />";
-
-  const text2 = "<Please keep your computer on />";
-
-  const messages = [text1, text2];
+  const fullText =
+    "\n<Loading...       \n<Loading some more...\n<it’s not broken, it’s just dramatic!\n<I swear its almost done!! wait!! don't close it!!\n/>";
 
   useEffect(() => {
-    let index = 0;
-    let fullText = messages[step];
+    let index = -1;
 
     const interval = setInterval(() => {
-      setText(fullText.substring(0, index));
       index++;
+      setText((preText) => preText + fullText[index]);
 
-      if (index > fullText.length) {
-        if (step < 1) {
-          clearInterval(interval);
-          setTimeout(() => {
-            setStep(step + 1);
-          }, 1000);
-        } else {
-          clearInterval(interval);
-          setTimeout(() => {
-            clearInterval(interval);
-            onComplete();
-          }, 3000);
-        }
+      if (index === fullText.length - 1) {
+        clearInterval(interval);
+        setTimeout(() => {
+          onComplete();
+        }, 3000);
       }
-    }, 1);
+    }, 100);
 
-    return () => clearInterval(interval);
-  }, [onComplete, step]);
+    // Keyboard event handler
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter" || event.key === " " /* space */) {
+        onComplete();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onComplete]);
 
   return (
-    <div className="fixed inset-0 z-50 bg-black text-grey-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold">
-        {text} <span className="animate-blink ml-1"> | </span>
-      </div>
+    <div className="fixed inset-0 z-50 bg-black text-grey-100 relative">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="whitespace-pre-line mb-4 text-4xl font-mono font-bold">
+          {text} <span className="animate-blink ml-1"> | </span>
+        </div>
 
-      <div className="w-[200px] h-[2px] bg-grey-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15_#3b82f6] animate-loading-bar">
-          {" "}
+        <div className="w-[200px] h-[2px] bg-grey-800 rounded relative overflow-hidden mb-6">
+          <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15_#3b82f6] animate-loading-bar" />
         </div>
       </div>
+
+      <button
+        className="absolute bottom-4 right-4 border border-orange-600/60 text-orange-600 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(202,76,30,0.1)] hover:bg-orange-500/10 cursor-pointer"
+        onClick={() => onComplete()}
+      >
+        skip animation
+      </button>
     </div>
   );
 };
