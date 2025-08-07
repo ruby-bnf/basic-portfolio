5657c556fab4a2255703c5d70349247f1ec35d1f
Ruby bnf <r.barnerfontyn@gmail.com>
2025-08-07 22:32:32 +0200

    Add about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,14 +1,137 @@
 export const About = () => {
   return (
     <section
-      id-about
+      id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
       <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-red-500 to-pink-700 bg-clip-text text-transparent text-center">
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-[#e1144c] to-pink-700 bg-clip-text text-transparent text-center">
           About Me
         </h2>
+
+        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+          <p className="text-grey-300 mb-6">
+            I'm a front-end developer, designer, and artist - which is a fancy
+            way of saying I like making things look good and work well.{" "}
+          </p>
+          <Skills />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          <Education />
+          <WorkExpirience />
+        </div>
       </div>
     </section>
+  );
+};
+
+const Skills = (skills) => {
+  const frontEndSkills = [
+    "React",
+    "TypeScript",
+    "Tailwing",
+    "JavaScript",
+    "C#",
+    "CSS",
+    "HTML",
+  ];
+
+  const backEndSkills = ["Python", "C", "C++", "Java", "Scala"];
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <SkillsList name={"Front end"} skills={frontEndSkills} />
+      <SkillsList name={"Back end"} skills={backEndSkills} />
+    </div>
+  );
+};
+
+const SkillsList = ({ name, skills }) => {
+  return (
+    <div className="rounded-xl p-6 hover:-translate-y-1 transition-aall">
+      <h3 className="text-xl font-bold mb-4">{name}</h3>
+      <div className="flex flex-wrap gap2">
+        {skills.map((tech, key) => {
+          return (
+            <span
+              key={key}
+              className="bg-pink-700/10 text-pink-700 py-1 px-3 rounded-full text-sm hover:bg-[#e1144c]/20  transition"
+            >
+              {tech}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Education = () => {
+  const Study = ({ course, institution, date }) => {
+    return (
+      <span>
+        <span className="text-pink-700 font-semibold">{course}</span>{" "}
+        <span className="text-xs">{` - ${institution} (${date})`}</span>
+      </span>
+    );
+  };
+
+  return (
+    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+      <h3 className="text-xl font-bold mb-4">Education</h3>
+      <ul className="list-disc list-inside text-grey-300 space-y-2">
+        <li>
+          <Study
+            institution={"Vrije Universiteit Amsterdam"}
+            date={"2017 - 2022"}
+            course={"B.S. in computer Science"}
+          />
+        </li>
+
+        <li>
+          <Study
+            institution={"Design and Illustration Study"}
+            date={"2022 - 2024"}
+            course={"Willem de Kooning Academie"}
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const WorkExpirience = () => {
+  const Position = ({ company, date, description }) => {
+    return (
+      <div className="space-grey-4 text-grey-100 mb-4">
+        <h4 className="text-pink-700 font-semibold">{`${company} (${date})`}</h4>
+        <p className="whitespace-pre-line text-xs">{description}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+      <h3 className="text-xl font-bold mb-4">Work Experience</h3>
+      <div className="space-grey-4 text-grey-100">
+        <div>
+          <Position
+            company={"Software engineer at Mendix"}
+            date={"2025 - Present"}
+            description={
+              "AI - Maintenance and development of the Artificial Intelligence. \nUser experience - Maintenance and improvement of the front-end (UX/UI)."
+            }
+          />
+
+          <Position
+            company={"Teaching Assistant at Vrije Universiteit Amsterdam"}
+            date={"2018 - 2022"}
+            description={
+              "Teaching and mentoring in University-level computer science courses such as advanced Programming, operating systems and Computer Networks."
+            }
+          />
+        </div>
+      </div>
+    </div>
   );
 };
